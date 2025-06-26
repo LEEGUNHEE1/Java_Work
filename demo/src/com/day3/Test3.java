ddeb8be613a40b269ec58eb26553e14d40a7b11c
LEEGUNHEE1 <leguhe0107@gmail.com>
2025-06-26 09:39:43 +0900

    day3.Test3 modify

--- demo/src/com/day3/Test3.java
+++ demo/src/com/day3/Test3.java
@@ -1,14 +1,11 @@
 package com.day3;
 
-import java.io.BufferedReader;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Test3 {
 
 	public static void main(String[] args) throws IOException{
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		
 		char ch, result;
